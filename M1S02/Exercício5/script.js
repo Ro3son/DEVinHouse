8b60b1795f00a00d2b76419f85ac6bbfadd30677
Robson De Andrade <theairelibre@protonmail.com>
2022-03-09 13:05:06 -0300

    Add script.js ex5

--- M1S02/Exercício5/script.js
+++ M1S02/Exercício5/script.js
@@ -1,1 +1,10 @@
-Exercício5
+// Exercício5
+/*
+Calcule a área do triângulo
+A = (base * altura) / 2
+base 12cm e altura 8cm
+*/
+
+var base = 12, altura = 8;
+var area = (base * altura) / 2;
+console.log(area); // 48
