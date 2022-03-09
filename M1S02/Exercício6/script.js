37d0a06c69772eab595701a05011fdbeee13d7af
Robson De Andrade <theairelibre@protonmail.com>
2022-03-09 13:43:34 -0300

    change formatting script ex6

--- M1S02/Exercício6/script.js
+++ M1S02/Exercício6/script.js
@@ -7,7 +7,7 @@ pi igual a 3.14, raio 3cm, altura 7cm
 */
 
 var pi = 3.14,
-  raio = 3,
-  altura = 7;
-var volumeCilindro = pi * Math.pow(raio, 2) * altura;
+    raio = 3,
+    altura = 7;
+var volumeCilindro = (pi * Math.pow(raio, 2) * altura);
 console.log(volumeCilindro.toFixed(2)); //197.82
