233a1f3268920e05ca0b65216b6767e518aab5a5
Robson De Andrade <theairelibre@protonmail.com>
2022-03-09 15:07:58 -0300

    Add script ex8

--- M1S02/Exercício8/script.js
+++ M1S02/Exercício8/script.js
@@ -1,1 +1,14 @@
-Exercício8
+//Exercício8
+
+var arr = [56, true, 'JS', 67, 99, 12, false, 88];
+
+// a) Imprima o valor do primeiro elemento;
+console.log(arr[0]); // 56
+// b) Imprima o valor do último elemento;
+console.log(arr[7]); // 88 
+// c) Imprima o valor do terceiro elemento;
+console.log(arr[2]); // JS
+// d) Imprima o tipo ("typeof") do valor do quinto elemento.
+console.log(typeof (arr[4])); // number
+
+
