5e06f9f2159e38c364ef5313f63905957dc22c90
Robson De Andrade <theairelibre@protonmail.com>
2022-03-09 14:42:49 -0300

    Add script.js ex7

--- M1S02/Exercício7/script.js
+++ M1S02/Exercício7/script.js
@@ -1,1 +1,24 @@
-Exercício7
+//Exercício7
+
+/*
+Calcule a quantidade de pontos e de jogos do time Tabajara F.C
+Sendo que: 
+Vitória = 3 pontos
+Derrota = 0 pontos
+Empate = 1 ponto
+
+O time Tabajara F.C tem:
+12 vitórias
+2 derrotas 
+6 empates
+
+*/
+var vitoria = 3,
+    derrota = 0,
+    empate = 1;
+var tabajaraVitorias = 12,
+    tabajaraDerrotas = 2,
+    tabajaraEmpates = 6;
+var numPontos = ((tabajaraVitorias * vitoria) + (tabajaraDerrotas * derrota) + (tabajaraEmpates * empate)); // 42 pontos
+var numPartidas = ((tabajaraVitorias) + (tabajaraDerrotas) + (tabajaraEmpates)); // 20 partidas
+console.log("O time Tabajara F.C acumulou " + numPontos + " pontos após jogar " + numPartidas + " partidas.");
