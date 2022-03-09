a7d5ad96ce49a156cc2a3977bee7817deb48bc45
Robson De Andrade <theairelibre@protonmail.com>
2022-03-09 09:26:54 -0300

    Add script.js ex3

---
+++ M1S02/Exercício3/script.js
@@ -0,0 +1,4 @@
+// Exercício3
+var respostaUsuario = window.confirm("Tem certeza que deseja sair do sistema?");
+console.log(respostaUsuario); // true
+console.log(typeof respostaUsuario); // boolean
