1014c8c53f34d384f7ec26f4a05ce13839e228de
Robson De Andrade <theairelibre@protonmail.com>
2022-03-09 13:41:02 -0300

    Add script.js ex6

--- M1S02/Exercício6/script.js
+++ M1S02/Exercício6/script.js
@@ -1,1 +1,13 @@
-Exercício6
+//Exercício6
+
+/*
+Calcular o volume do cilindro
+V = pi * r**2 * h, sendo
+pi igual a 3.14, raio 3cm, altura 7cm
+*/
+
+var pi = 3.14,
+  raio = 3,
+  altura = 7;
+var volumeCilindro = pi * Math.pow(raio, 2) * altura;
+console.log(volumeCilindro.toFixed(2)); //197.82
