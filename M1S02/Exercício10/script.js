f1f44686ec7cb3e4b1de812da25eff06a5e810b7
Robson De Andrade <theairelibre@protonmail.com>
2022-03-11 15:49:46 -0300

    Update script.js ex10

--- M1S02/Exercício10/script.js
+++ M1S02/Exercício10/script.js
@@ -1,18 +1,14 @@
 // Exercício10
-/*
-Débito: valores negativos
-Crédito: valores positivos
-*/
 
 var extrato = [100, -35, -15, -5, 55, -20];
 
 // a) Percorra o array (operador 'for') e imprima cada elemento do array na ordem;
 
-for (var i = 0; i < extrato.length; i++) {// índices: 0 1 2 3 4 5
+for (var i = 0; i < extrato.length; i++) {
   console.log(extrato[i]); // 100, -35, -15, -5, 55, -20
 }
-
 // b) Percorra o array (operador 'for') e imprima os elementos na ordem inversa (de trás pra frente);
+
 for (var i = 0; i < 1; i++) {
   var ordemInversa = extrato;
   ordemInversa.reverse();
@@ -21,18 +17,18 @@ for (var i = 0; i < 1; i++) {
 
 /* c) Percorra o array (operador 'for') e calcule o saldo final da conta (valor inicial era zero),
 e imprima o resultado no console. (* Dica: crie uma variável 'saldo' inicializada com zero e vá atualizando seu valor);*/
-var saldo = 0; // saldo inicial
-saldo = 100; // saldo disponível
 
-var arr = [];
-
-for (var i = 0; i < extrato.length; i++) {
-  var x = extrato[i];
-
-  if (x < 0) {
-    arr.length++;
+var saldo = 0;
+saldo = 100;
+extrato.sort((a, b) => b - a); // [ 100, 55, -5, -15, -20, -35 ]
+for (var i = 0; i < extrato.length - 3; i++) {
+  if (extrato[i] < 0) {
+    var debito = extrato.slice(2, 6); // [ -5, -15, -20, -35 ]
+    var somaDebito = (debito[0] + debito[1] + debito[2] + debito[3]); // -75
+    var resultadoSaldo = (saldo + somaDebito);
+// d) Imprima no console a mensagem 'saldo positivo' ou 'saldo negativo', dependendo do resultado do saldo final calculado.
+    var resultadoSaldoFinal = resultadoSaldo > 0 ? "saldo positivo" : "saldo negativo";
   }
 }
-console.log(arr.length); // 4 números negativos
-
-// d) Imprima no console a mensagem 'saldo positivo' ou 'saldo negativo', dependendo do resultado do saldo final calculado.
+console.log(`Saldo Final: ${resultadoSaldo} reais. 
+${resultadoSaldoFinal}`);
