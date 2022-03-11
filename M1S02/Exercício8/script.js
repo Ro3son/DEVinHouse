bd7dbef28f0cfbca745ea23cdf53d95234401642
Robson De Andrade <theairelibre@protonmail.com>
2022-03-11 19:31:03 -0300

    update script.js ex8

--- M1S02/Exercício8/script.js
+++ M1S02/Exercício8/script.js
@@ -5,10 +5,11 @@ var arr = [56, true, 'JS', 67, 99, 12, false, 88];
 // a) Imprima o valor do primeiro elemento;
 console.log(arr[0]); // 56
 // b) Imprima o valor do último elemento;
-console.log(arr[7]); // 88 
+console.log(arr.length - 1); // 88 
 // c) Imprima o valor do terceiro elemento;
 console.log(arr[2]); // JS
 // d) Imprima o tipo ("typeof") do valor do quinto elemento.
-console.log(typeof (arr[4])); // number
+console.log(typeof(arr[4])); // number
 
-
+// se vc usar length vai ter que usar -1 pq o length conta que nem a gente
+// a maquina conta a partir de 0
