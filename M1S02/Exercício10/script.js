723d999a700b7ec76f537a2fefff197550099ae9
Robson De Andrade <theairelibre@protonmail.com>
2022-03-11 11:09:24 -0300

    Update script ex10

--- M1S02/Exercício10/script.js
+++ M1S02/Exercício10/script.js
@@ -3,28 +3,36 @@
 Débito: valores negativos
 Crédito: valores positivos
 */
+
 var extrato = [100, -35, -15, -5, 55, -20];
 
 // a) Percorra o array (operador 'for') e imprima cada elemento do array na ordem;
 
-// for (let i = 0; i < 1; i++) {
-//     var ordem = extrato;
-//     ordem.sort((a, b) => (b - a ? 1 : -1));
-//     console.log(`${ordem[0] + "\n" + ordem[1] + "\n" + ordem[2] +
-//         "\n" + ordem[3] + "\n" + ordem[4] + "\n" + ordem[5]}`); // 100, -35, -15, -5, 55, -20
-// }
+for (var i = 0; i < extrato.length; i++) {// índices: 0 1 2 3 4 5
+  console.log(extrato[i]); // 100, -35, -15, -5, 55, -20
+}
 
 // b) Percorra o array (operador 'for') e imprima os elementos na ordem inversa (de trás pra frente);
-
-// for (let i = 0; i < 1; i++) {
-//     var ordemInversa = extrato;
-//     ordemInversa.reverse();
-//     console.log(`${ordemInversa[0] + "\n" + ordemInversa[1] + "\n" + ordemInversa[2] +
-//         "\n" + ordemInversa[3] + "\n" + ordemInversa[4] + "\n" + ordemInversa[5]}`); // -20, 55, -5, -15, -35, 100
-// }
+for (var i = 0; i < 1; i++) {
+  var ordemInversa = extrato;
+  ordemInversa.reverse();
+  console.log(ordemInversa); // -20, 55, -5, -15, -35, 100
+}
 
 /* c) Percorra o array (operador 'for') e calcule o saldo final da conta (valor inicial era zero),
-e imprima o resultado no console.
-(* Dica: crie uma variável 'saldo' inicializada com zero e vá atualizando seu valor);*/
+e imprima o resultado no console. (* Dica: crie uma variável 'saldo' inicializada com zero e vá atualizando seu valor);*/
+var saldo = 0; // saldo inicial
+saldo = 100; // saldo disponível
+
+var arr = [];
+
+for (var i = 0; i < extrato.length; i++) {
+  var x = extrato[i];
+
+  if (x < 0) {
+    arr.length++;
+  }
+}
+console.log(arr.length); // 4 números negativos
 
 // d) Imprima no console a mensagem 'saldo positivo' ou 'saldo negativo', dependendo do resultado do saldo final calculado.
