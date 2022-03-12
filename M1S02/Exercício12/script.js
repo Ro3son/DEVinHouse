199e399f6f6f21517eb4521f82028203e325710d
Robson De Andrade <theairelibre@protonmail.com>
2022-03-12 10:33:10 -0300

    script ex12

--- M1S02/Exercício12/script.js
+++ M1S02/Exercício12/script.js
@@ -1,1 +1,22 @@
 //Exercício12
+var arr = [12, 67, 23, 32];
+
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for (var x of arr) {
+    console.log(x);
+}
+
+var y = 0;
+while (y < arr.length) {
+    console.log(arr[y]);
+    y++;
+}
+
+var i = 0;
+do {
+    console.log(arr[i]);
+    i++;
+} while (i < arr.length);
