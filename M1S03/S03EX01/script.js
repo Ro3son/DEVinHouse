c7ada5a086efa37c22bdfdf65f33788bf348f9e4
Robson De Andrade <theairelibre@protonmail.com>
2022-03-15 11:31:11 -0300

    functions temp conversion

--- M1S03/S03EX01/script.js
+++ M1S03/S03EX01/script.js
@@ -1,1 +1,13 @@
-S03EX01
+//S03EX01
+
+function ctof(celsius) {
+    celsius = (celsius * 9) / 5 + 32;
+    console.log(`${celsius}°F`);
+}
+ctof(0); // 32°F
+
+function ftoc(fahrenheit) {
+    fahrenheit = (fahrenheit - 32) * (5 / 9);
+    console.log(`${fahrenheit}°C`);
+}
+ftoc(32); // 0°F
