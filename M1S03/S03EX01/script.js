51426054e56437585dae7bb7c4139bc73014b9ec
Robson De Andrade <theairelibre@protonmail.com>
2022-03-16 08:58:27 -0300

    update script ex01

--- M1S03/S03EX01/script.js
+++ M1S03/S03EX01/script.js
@@ -1,7 +1,7 @@
 //S03EX01
 
 function celsiusToFahrenheit(celsius) {
-    celsius = (celsius * 9) / 5 + 32;
+    celsius = (celsius * 9) / 5 + 32;    // constantes dentro da função
     console.log(`${celsius}°F`);
 }
 celsiusToFahrenheit(77); // 170.6°F
@@ -10,4 +10,22 @@ function fahrenheitToCelsius(fahrenheit) {
     fahrenheit = (fahrenheit - 32) * (5 / 9);
     console.log(`${fahrenheit}°C`);
 }
-fahrenheitToCelsius(77); // 25°
+fahrenheitToCelsius(77); // 25°C
+
+// var celsius = 77;
+// var toFahrenheit = (celsius * 9) / 5 + 32;  // Constantes fora da função
+
+// function celsiusToFahrenheit() {
+//     var resultado = toFahrenheit;
+//     console.log(`${resultado}°F`);
+// }
+// celsiusToFahrenheit();
+
+// var fahrenheit = 77;
+// var toCelsius = (fahrenheit - 32) * (5 / 9);
+
+// function fahrenheitToCelsius() {
+//     var resultado = toCelsius;
+//     console.log(`${resultado}°C`);
+// }
+// fahrenheitToCelsius();
