879117190ff574e68d2c6007bc835d5fc23587cd
Robson De Andrade <theairelibre@protonmail.com>
2022-03-16 15:02:28 -0300

    DOM createElement divs

--- M1S03/S03EX04/script.js
+++ M1S03/S03EX04/script.js
@@ -1,1 +1,14 @@
-s03ex04
+// Create Divs //
+const div1 = document.createElement('div'),
+    div2 = document.createElement('div'),
+    div3 = document.createElement('div');
+
+div1.id = 'galeria';
+div2.id = 'vídeo';
+div3.id = 'perfil';
+
+document.body.append(div1);
+document.body.append(div2);
+document.body.append(div3);
+
+
