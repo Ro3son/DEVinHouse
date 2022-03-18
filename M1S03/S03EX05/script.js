20d6d29cf54a3c935a38f4c6d4ed9e6a4f94e4c8
Robson De Andrade <theairelibre@protonmail.com>
2022-03-18 16:10:00 -0300

    DOM create element input addEventListerner *almost there

--- M1S03/S03EX05/script.js
+++ M1S03/S03EX05/script.js
@@ -1,1 +1,22 @@
-s03ex05
+const input = document.createElement("input");
+input.type = "text";
+input.id = "input";
+input.style.border = "2px solid #000";
+document.body.append(input);
+
+const inputChange = document.getElementById("input");
+inputChange.addEventListener("input", changeColor);
+
+function changeColor() {
+  if (input.value === "a") {
+    document.body.style.backgroundColor = "yellow";
+  } else if (input.value === "b") {
+    document.body.style.backgroundColor = "black";
+  } else if ("[enter]") {
+    document.body.style.background = "";
+    document.querySelector("input").style.padding = "";
+  }
+  if ("[space]") {
+    document.querySelector("input").style = "padding: 20px; font-size: 22px";
+  }
+}
