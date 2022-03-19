66f25e671b939840ed770460345fe5c0db7bf67d
Robson De Andrade <theairelibre@protonmail.com>
2022-03-19 14:42:48 -0300

    DOM keyboard event.key keycode

--- M1S03/S03EX05/script.js
+++ M1S03/S03EX05/script.js
@@ -4,19 +4,19 @@ input.id = "input";
 input.style.border = "2px solid #000";
 document.body.append(input);
 
-const inputChange = document.getElementById("input");
-inputChange.addEventListener("input", changeColor);
-
-function changeColor() {
-  if (input.value === "a") {
+function changeColor(event) {
+  if (event.key == "a") {
     document.body.style.backgroundColor = "yellow";
-  } else if (input.value === "b") {
+  } else if (event.key == "p") {
     document.body.style.backgroundColor = "black";
-  } else if ("[enter]") {
-    document.body.style.background = "";
-    document.querySelector("input").style.padding = "";
-  }
-  if ("[space]") {
-    document.querySelector("input").style = "padding: 20px; font-size: 22px";
+  } else if (event.key == " ") { // space
+    input.style.padding = "20px";
+    input.style.fontSize = "22px";
+  } else if (event.key == "Enter") {
+    document.body.style.backgroundColor = "";
+    input.style.removeProperty('padding');
+    input.style.removeProperty('font-size');
   }
 }
+const inputChange = document.getElementById("input");
+inputChange.addEventListener("keypress", changeColor);
