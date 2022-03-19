bbd12c2ab46daa3bab22b75666d7dc97449ce33e
Robson De Andrade <theairelibre@protonmail.com>
2022-03-19 15:29:17 -0300

    update ex10c

--- M1S02/Exercício10/script.js
+++ M1S02/Exercício10/script.js
@@ -19,19 +19,31 @@ for (var i = 0; i < 1; i++) {
 e imprima o resultado no console. (* Dica: crie uma variável 'saldo' inicializada com zero e vá atualizando seu valor);*/
 
 var saldo = 0;
-saldo = 100;
 
 extrato.sort((a, b) => b - a); // [ 100, 55, -5, -15, -20, -35 ]
 
-for (var i = 0; i < extrato.length - 3; i++) {
+for (var i = 0; i < extrato.length; i++) {
+  
   if (extrato[i] < 0) {
+    var credito = extrato.slice(0, 2); // [100, 55]
+    var somaCredito = (credito[0] + credito[1]); // 155
+
     var debito = extrato.slice(2, 6); // [ -5, -15, -20, -35 ]
     var somaDebito = (debito[0] + debito[1] + debito[2] + debito[3]); // -75
-    var resultadoSaldo = (saldo + somaDebito);
+
+    var resultadoSaldo = (somaCredito + somaDebito); // 80 reais
 // d) Imprima no console a mensagem 'saldo positivo' ou 'saldo negativo', dependendo do resultado do saldo final calculado.
     var resultadoSaldoFinal = resultadoSaldo > 0 ? "saldo positivo" : "saldo negativo";
   }
 }
-
 console.log(`Saldo Final: ${resultadoSaldo} reais. 
 ${resultadoSaldoFinal}`);
+
+//Solution II//
+var saldo2 = 0;
+
+extrato.sort((a, b) => b - a); // [ 100, 55, -5, -15, -20, -35 ]
+for (var i = 0; i < extrato.length; i++) {
+  saldo2 = saldo2 + extrato[i];
+}
+console.log(saldo2);
