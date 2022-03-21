36c614f3ab91a70d791cde4b79f3e4eaaa88394e
Robson De Andrade <theairelibre@protonmail.com>
2022-03-21 11:58:34 -0300

    for loop setAttribute

--- M1S03/S03EX06/script.js
+++ M1S03/S03EX06/script.js
@@ -1,5 +1,5 @@
 // URL da API //
-const requestURL = 'https://dog.ceo/api/breeds/image/random/1';
+const requestURL = 'https://dog.ceo/api/breeds/image/random/10'; // Exibe 10 imagens //
 
 // Fecth()method para buscar dados da API //
 fetch(requestURL)
@@ -10,11 +10,12 @@ fetch(requestURL)
     let arr = response.message;
     for (let i = 0; i < arr.length; i++) {
       const img = document.createElement('img');
-      img.src = response.message;
-      img.setAttribute('style', 'width: 50%; height: 400px');
+      img.src = response.message[i];
+      img.setAttribute('style', 'width: 120%; height: 120px');
       document.getElementById("containerDogs").appendChild(img);
     }
   });
 // Tipo de solicitação: GET //
 // Get 404 //
 // Could not load the image //
+// Solução posição[i] no loop //
