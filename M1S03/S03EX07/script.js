6288e3663725257b4ef64de822b2142c5671fb6f
Robson De Andrade <theairelibre@protonmail.com>
2022-03-21 15:19:12 -0300

    fetch API functions DOM

--- M1S03/S03EX07/script.js
+++ M1S03/S03EX07/script.js
@@ -1,1 +1,31 @@
-s03ex07
+function projecaoPopulacaoBR() {
+  const requestURL =
+    "https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/BR";
+
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const main = document.createElement("main"),
+        h1 = document.createElement("h1");
+      p = document.createElement("p");
+      h1.innerText = "Projeção População Brasileira: ";
+      h1.setAttribute(
+        "style",
+        "text-align: center; font-family: monospace; color: #ffffff"
+      );
+      p.innerText = `${data.projecao.populacao}`;
+      p.setAttribute(
+        "style",
+        "font-size: 2.4em; text-align: center; font-family: monospace; color: #ffffff"
+      );
+      document.querySelector("body").append(main);
+      document.querySelector("main").append(h1);
+      document.querySelector("main").append(p);
+      document.body.style.backgroundImage =
+        "linear-gradient(to left, #118d2f, #f5df00, #193392)";
+      document.body.style.padding = "200px";
+    });
+}
+projecaoPopulacaoBR();
