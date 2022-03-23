c527e170a5dc6d5e4468425db6c5adaf78e931ec
Robson De Andrade <theairelibre@protonmail.com>
2022-03-23 17:19:04 -0300

    DOM function concat variable if else if statement

--- M1S03/S03EX08/script.js
+++ M1S03/S03EX08/script.js
@@ -19,7 +19,7 @@ function projecaoPopulacaoBR() {
                 option3 = document.createElement("option"),
                 option4 = document.createElement("option"),
                 option5 = document.createElement("option");
-            
+
             h1.innerText = "Projeção População Brasileira: ";
             h1.setAttribute("style", "text-align: center; font-family: monospace; color: #ffffff");
 
@@ -34,23 +34,18 @@ function projecaoPopulacaoBR() {
             select.setAttribute("style", "font-size: 0.7em; font-family: monospace; border: 1px solid #000; color: #004282");
 
             option1.innerText = "Centro-Oeste";
-            option1.id = "centro-oeste";
             option1.value = "1";
 
             option2.innerText = "Nordeste";
-            option2.id = "nordeste";
             option2.value = "2";
 
             option3.innerText = "Sudeste";
-            option3.id = "sudeste";
             option3.value = "3";
 
             option4.innerText = "Sul";
-            option4.id = "sul";
             option4.value = "4";
 
             option5.innerText = "Norte";
-            option5.id = "norte";
             option5.value = "5";
 
             document.querySelector("body").append(main);
@@ -66,18 +61,44 @@ function projecaoPopulacaoBR() {
             document.querySelector("select").append(option4);
             document.querySelector("select").append(option5);
 
-            document.body.style.padding = "200px";
-            document.body.style.backgroundImage = "linear-gradient(to left, #118d2f, #f5df00, #193392)";
+            let localidade = document.querySelector("#localidade");
+            localidade.addEventListener("change", projecaoPopulacaoRegiao);
 
-            const eventClick = document.getElementById("centro-oeste");
-            eventClick.addEventListener("click", optionRegion);
+            function projecaoPopulacaoRegiao() {
+                const requestURL = "https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/" + (localidade.value);
+                fetch(requestURL)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        let p2 = document.createElement("p");
+                        p2.setAttribute("style", "font-size: 1.2em; font-family: monospace; padding: 12px; text-align: center");
 
-            function optionRegion() {
-                let p2 = document.createElement("p");
-                p2.innerText = `Projeção Populacional Centro-Oeste: ${data.populacao}`;
-                p2.setAttribute("style", "font-size: 1.2em; font-family: monospace; padding: 12px; text-align: center");
-                document.querySelector("main").append(p2);
+                        let dataProjecoesPopulacao = data.projecao.populacao;
+
+                        if (localidade.value == 1) {
+                            p2.innerText = `Projeção Para a Região Centro-Oeste é ${dataProjecoesPopulacao}`;
+                        }
+                        else if (localidade.value == 2) {
+                            p2.innerText = `Projeção Para a Região Nordeste é ${dataProjecoesPopulacao}`;
+                        }
+                        else if (localidade.value == 3) {
+                            p2.innerText = `Projeção Para a Região Sudeste é ${dataProjecoesPopulacao}`;
+                        }
+                        else if (localidade.value == 4) {
+                            p2.innerText = `Projeção Para a Região Sul é ${dataProjecoesPopulacao}`;
+                        }
+                        else if (localidade.value == 5) {
+                            p2.innerText = `Projeção Para a Região Norte é ${dataProjecoesPopulacao}`;
+                        }
+
+                        document.querySelector("main").append(p2);
+                        document.body.style.paddingTop = '16px';
+                        document.body.style.backgroundImage = "linear-gradient(to left, #118d2f, #f5df00, #193392)";
+                    });
             }
+            projecaoPopulacaoRegiao();
         });
 }
 projecaoPopulacaoBR();
+
