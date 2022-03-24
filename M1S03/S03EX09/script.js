940a8fa91e44c747cf35cd4841f66377f0da38d1
Robson De Andrade <theairelibre@protonmail.com>
2022-03-24 11:04:32 -0300

    Array Fetch API undefined

--- M1S03/S03EX09/script.js
+++ M1S03/S03EX09/script.js
@@ -1,1 +1,31 @@
-s03ex09
+const nomesFamiliares = ['Robson', 'Kamila', 'Iolanda', 'Cicero', 'Braulina'];
+
+nomesFamiliares.forEach( function(element) {
+    const li = document.createElement('li');
+    li.setAttribute('value', element);
+    li.innerText = element;
+    document.querySelector('ul').append(li);
+})
+
+let selector = document.querySelector("#list");
+
+const requestURL = `https://servicodados.ibge.gov.br/api/v2/censos/nomes/${selector.type}`;
+
+fetch(requestURL)
+.then( function(response) {
+    return response.json();
+})
+.then( function(data) {
+
+    const p = document.createElement('p');
+
+    let censoNomes = data.nomes;
+
+    console.log(censoNomes);
+   
+    if (selector.type == 'Robson') {
+        p.innerText = `${censoNomes}`;
+    }
+    document.body.appendChild(p);
+})
+
