0bc4d17537f6cf4cfb72adac4076aca906b0f9f3
Robson De Andrade <theairelibre@protonmail.com>
2022-03-25 11:40:40 -0300

    requestURL doesn't work

--- M1S03/S03EX09/script.js
+++ M1S03/S03EX09/script.js
@@ -1,33 +1,58 @@
-const nomesFamiliares = ['Robson'];
+// Variável constante recebe array de strings //
+const nomesDeFamiliares = ["Robson", "Iolanda", "Cicero", "Artur", "Braulina", "João", "Kamila"];
 
-nomesFamiliares.forEach(function (element, index) {
-    const li = document.createElement('li');
+let list = document.querySelector("#list");
+
+// Criando vários elementos <li> //
+nomesDeFamiliares.forEach(function (element, index) {
+    const li = document.createElement("li");
     li.value = index;
     li.innerText = element;
-    document.querySelector('ul').append(li);
-})
+    li.setAttribute("style", "list-style-type: none; font-size: 2em; font-family: monospace");
+    document.querySelector("ul").append(li);
+});
 
-let selector = document.querySelector("#list");
-
-const requestURL = `https://servicodados.ibge.gov.br/api/v2/censos/nomes/${selector.innerText}`;
+const requestURL = "https://servicodados.ibge.gov.br/api/v2/censos/nomes/";
 
 fetch(requestURL)
     .then(function (response) {
         return response.json();
     })
-    .then(function (data) {
-
-        let dataNomes = data;
+    .then(function (json) {
+        
+        let dataNomes = json;
 
         console.log(dataNomes);
 
-        for (object of data) {
-            const p = document.createElement('p');
-
-            if (selector.innerText == 'Robson') {
-                p.innerText = `Frequencia: ${object.res[0].frequencia}`;
-            }
-            document.querySelector('ul').append(p);
-        }
-    })
-
+        // Chart.js //
+        const labels = [
+            "1030",
+            "1930,1940",
+            "1040,1950",
+            "1050,1060",
+            "1060,1970",
+            "1970,1980",
+            "1980,1990",
+            "1990,2000",
+            "2000,2010",
+        ];
+        const data = {
+            labels: labels,
+            datasets: [
+                {
+                    label: "Evolução Histórica de Cada Nome",
+                    backgroundColor: "rgb(255, 99, 132)",
+                    borderColor: "rgb(255, 99, 132)",
+                    data: [0, 300000],
+                },
+            ],
+        };
+        const config = {
+            type: "line",
+            data: data,
+            options: {
+                responsive: true,
+            },
+        };
+        const myChart = new Chart(document.getElementById("myChart"), config);
+    });
