a1a55835d73c812b5136c0a60a795c03e2f76bf9
Robson De Andrade <theairelibre@protonmail.com>
2022-03-27 13:54:11 -0300

    ex08 every method

--- M1S04/every.js
+++ M1S04/every.js
@@ -4,12 +4,19 @@ Use a função 'every' para identificar se todos os alunos foram aprovados no cu
 sabendo que a nota mínima para aprovação é 7 (inclusive).
 Imprima o resultado no console.
 
+// Se os elementos da minha array for maior ou igual a 7..
+Every() method deve retornar true, se satisfaz a condição, caso contrário retorna false.
 */
-const notas = [10, 9, 10, 7, 6, 8, 9, 10, 7, 9];
+const notas = [10, 9, 10, 7, 9, 6, 9, 10, 7, 9];
 
-const resultado = [];
+const resultado = notas.every( (nota) => (nota >= 7));
 
-notas.every(function(nota) {
-    return resultado.push(nota >= 7);
-});
 console.log(resultado);
+
+// const resultado = [];
+
+// notas.every(function(nota) {
+//     return resultado.push(nota >= 7);
+// });
+// console.log(resultado);
+
