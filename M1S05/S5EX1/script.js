fa29b0c8529cecaa1f4da1f574f3ed864ef32043
Robson De Andrade <theairelibre@protonmail.com>
2022-03-29 19:00:29 -0300

    DOM Array from forEach Arrow fn

--- M1S05/S5EX1/script.js
+++ M1S05/S5EX1/script.js
@@ -1,4 +1,4 @@
-document.body.setAttribute('style','background-color: #2a2d2f; font-size: 1em; font-family: sans-serif');
+document.body.setAttribute('style', 'background-color: #2a2d2f; font-size: 1em; font-family: sans-serif');
 
 const div = document.createElement('div');
 
@@ -9,7 +9,7 @@ document.querySelector('body').appendChild(div);
 
 const inputs = ['input1', 'input2', 'input3', 'input4'];
 
-inputs.forEach( () => {
+inputs.forEach(() => {
     const input = document.createElement('input');
     input.type = 'number';
     input.placeholder = 'Digite um número'
@@ -21,18 +21,33 @@ inputs.forEach( () => {
 const button = document.createElement('button');
 button.innerText = 'Disparar Ação';
 button.id = 'btn';
-button.setAttribute('style','padding: 10px; border: 2px solid #2475ff; cursor: pointer; color: #d9eee1; background-color: transparent');
+button.setAttribute('style', 'padding: 10px; border: 2px solid #2475ff; cursor: pointer; color: #d9eee1; background-color: transparent');
 document.querySelector('div').appendChild(button);
 
 let btn = document.getElementById('btn');
 btn.addEventListener('click', desabilitaElementosESoma);
 
 function desabilitaElementosESoma() {
-    const input = document.querySelectorAll('input');
-    input.forEach( (input) => input.disabled = true);
-    
+
+    let soma = 0;
+
+    const input = document.querySelectorAll('input'); // nodeCollection //
+
+    arrayInputs = Array.from(input);
+
+    arrayInputs.forEach((input, sum) => {
+        input.disabled = true;
+        sum = parseInt(input.value);
+        if (!isNaN(sum)) {
+            soma = soma + sum;
+        }
+    });
+    // arrayInputs.reduce(function(soma, atual) {
+    //     return soma + atual;
+    // }, 0);
+
     const h1 = document.createElement('h1');
     h1.setAttribute('style', 'padding: 4px; text-align: center; color: #d9eee1');
-    h1.innerText = `A soma é: ${inputs.value}`;
+    h1.innerText = `A soma é: ${soma}`;
     document.querySelector('body').append(h1);
 }
