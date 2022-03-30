719dd886a74e3e64be657b487319ff9acf339ce0
Robson De Andrade <theairelibre@protonmail.com>
2022-03-30 11:00:17 -0300

    DOM map method

--- M1S05/S5EX2/script.js
+++ M1S05/S5EX2/script.js
@@ -1,1 +1,41 @@
-s05ex2
+document.body.setAttribute('style', 'background-color: #2a2d2f; font-size: 1em; font-family: sans-serif');
+
+const div = document.createElement('div');
+
+div.id = 'flex-container';
+
+div.setAttribute('style', 'display: flex; justify-content: center; align-items: center; gap: 0.4em; height: 300px; padding: 16px');
+document.querySelector('body').appendChild(div);
+
+const inputs = ['input1', 'input2', 'input3', 'input4'];
+
+inputs.forEach(() => {
+    const input = document.createElement('input');
+    input.type = 'number';
+    input.placeholder = 'Digite um valor'
+    input.setAttribute('style', 'padding: 10px; border: 1px solid #00f8c0; color: #d9eee1; background-color: transparent');
+    document.querySelector('div').appendChild(input);
+
+});
+
+const button = document.createElement('button');
+button.innerText = 'Disparar Ação';
+button.id = 'btn';
+button.setAttribute('style', 'padding: 10px; border: 2px solid #2475ff; cursor: pointer; color: #d9eee1; background-color: transparent');
+document.querySelector('div').appendChild(button);
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', criaGrupoDeArrays);
+
+function criaGrupoDeArrays() {
+
+    const input = document.querySelectorAll('input'); // nodeCollection //
+
+    inputArray = Array.from(input);
+
+    inputArray.map(function(grupo1, grupo2) {
+        grupo1 = inputs.slice(0, 2); // [ "input1", "input2" ]
+        grupo2 = inputs.slice(2, 4); //[ "input3", "input4" ]
+        });
+}
+criaGrupoDeArrays();
