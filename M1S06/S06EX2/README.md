8ff524bf31b497e5103d50f82d3c810156610b7a
Robson De Andrade <theairelibre@protonmail.com>
2022-04-08 18:45:02 -0300

    delete files

--- M1S06/S06EX2/README.md
+++
@@ -1,2 +0,0 @@
-Escreva uma função chamada 'adicionarSomenteNumeros' que recebe um número indefinido de parâmetros (incluindo strings e numbers) e retorna a soma dos parâmetros numérico.
-Dica: Lembram do 'typeof()' ?
--- M1S06/S06EX2/script.js
+++
@@ -1,9 +0,0 @@
-const adicionarSomenteNumeros = (string, str, ...numbers) => {
-    let sum = 0;
-    
-    for (let number of numbers ) {
-        sum = sum + number;
-    };
-    return sum;
-}
-console.log(adicionarSomenteNumeros('Robson', 'Dev', 1, 2, 3)); // 6
