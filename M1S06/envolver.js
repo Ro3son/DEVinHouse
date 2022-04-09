35d884d76b7be93ef8b5e348d8e0e977723eb327
Robson De Andrade <theairelibre@protonmail.com>
2022-04-09 13:41:22 -0300

    rest e spread

--- M1S06/envolver.js
+++ M1S06/envolver.js
@@ -1,16 +1,16 @@
 /*
 Escreva uma função chamada 'envolver' que recebe uma quantidade indefinida de tags html (no formato string)
-como parâmetros, e retorne outro array cujo primeiro elemento é '<html>', seguido das tags recebidas como parâmetro e como último elemento '</html>'.
+como parâmetros, e retorne outro array cujo primeiro elemento é '<html>', 
+seguido das tags recebidas como parâmetro e como último elemento '</html>'.
 
 Exemplo de entrada:
 const r = envolver('<body>','<div>','<h1>','</h1>','</div>','</body>');
 console.log(r); 
-// saida esperada: ['<html>', '<body>','<div>','<h1>','</h1>',
+// saida esperada: ['<html>', '<body>','<div>','<h1>','</h1>', </div>','</body', '</html>'];
 */
+let string = '<html>';
 
-const arrayElemento = ['<html>'];
-
-const envolver = (...tags) => {
-  return [ arrayElemento, ...tags, arrayElemento ];
+function envolver(...str) {
+ return [string, ...str, string];
 }
-envolver(tags);
+console.log(envolver('<body>','<div>','<h1>','</h1>','</div>','</body>'));
